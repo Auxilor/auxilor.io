ebb9e816ebe789d0cf4edb7da2f8955a40ba22df
Auxilor <william.favierparsons1@gmail.com>
2021-06-18 16:28:30 +0100

    Start

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // eslint-disable-next-line react/jsx-filename-extension
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
